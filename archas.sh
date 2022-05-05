93bd4c2e93e1c60f63d5f8f91a1aec25e13b1a4c
sephid86 <sephid86@gmail.com>
2022-05-05 11:46:18 +0900

    220505

--- archas.sh
+++ archas.sh
@@ -142,7 +142,10 @@ su - ${userid} -c "git config --global core.editor vim"
 #동영상 재생 프로그램, 터미널, 음악 재생 프로그램 설치합니다. 
 pacman -Sy smplayer smplayer-skins smplayer-themes ffmpegthumbnailer gst-libav gst-plugins-ugly rhythmbox xfce4-terminal
 
+pacman -Sy libreoffice-fresh-ko gimp
+
 pacman -R gnome-terminal
+pacman -R gnome-software
 
 #AMD ATI 드라이버 설치합니다.
 #pacman -Syy xf86-video-ati xf86-video-amdgpu mesa vulkan-radeon lib32-vulkan-radeon mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver vulkan-icd-loader vulkan-tools
