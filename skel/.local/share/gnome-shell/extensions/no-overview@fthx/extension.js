75ece434399a70e8787311a32686fdb6fb0fb8b2
sephid86 <sephid86@gmail.com>
2022-05-11 14:03:08 +0900

    220511

---
+++ skel/.local/share/gnome-shell/extensions/no-overview@fthx/extension.js
@@ -0,0 +1,38 @@
+/*
+  No overview at start-up
+  Contributors: @fthx, @fmuellner
+  License: GPL v3
+*/
+
+
+const Main = imports.ui.main;
+
+
+class Extension {
+    constructor() {
+        this._realHasOverview = Main.sessionMode.hasOverview;
+    }
+
+    enable() {
+        if (!Main.layoutManager._startingUp) {
+            return;
+        }
+
+        Main.sessionMode.hasOverview = false;
+        Main.layoutManager.connect('startup-complete', () => {
+            Main.sessionMode.hasOverview = this._realHasOverview
+        });
+        // handle Ubuntu's method
+        if (Main.layoutManager.startInOverview) {
+            Main.layoutManager.startInOverview = false;
+        }
+    }
+
+    disable() {
+        Main.sessionMode.hasOverview = this._realHasOverview;
+    }
+}
+
+function init() {
+	return new Extension();
+}
