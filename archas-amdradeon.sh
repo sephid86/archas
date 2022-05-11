c50a9fa1217e0e241274802073987b0abace056e
sephid86 <sephid86@gmail.com>
2022-05-11 18:47:07 +0000

    220511

--- archas-amdradeon.sh
+++ archas-amdradeon.sh
@@ -44,8 +44,8 @@ fi
 read
 #시간설정
 #hwclock -s
-timedatectl set-timezone Asia/Seoul
-#ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+#timedatectl set-timezone Asia/Seoul
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 #timedatectl set-local-rtc 1
 timedatectl set-ntp true
 #timedatectl set-local-rtc 1 --adjust-system-clock
