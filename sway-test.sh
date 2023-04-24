720a2864d6d89c5524ff1b45a0f2edb66d63bd27
sephid86 <sephid86@gmail.com>
2023-04-25 00:49:44 +0900

    230424

--- sway-test.sh
+++ sway-test.sh
@@ -134,9 +134,13 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 LC_ALL=C grub-mkconfig -o /boot/grub/grub.cfg
 
 #--- gnome 설치
-pacman -Sy --noconfirm wireplumber pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse pipewire-v4l2 pipewire-x11-bell lib32-pipewire lib32-pipewire-jack lib32-pipewire-v4l2 gst-plugin-pipewire sway ibus-hangul noto-fonts noto-fonts-cjk noto-fonts-emoji xdg-desktop-portal-wlr smplayer smplayer-skins smplayer-themes ffmpegthumbnailer gst-libav gst-plugins-good gst-plugin-va rhythmbox xfce4-terminal libreoffice-fresh-ko gimp pavucontrol firefox-i18n-ko greetd grim slurp
+pacman -Sy --noconfirm wireplumber pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse pipewire-v4l2 pipewire-x11-bell lib32-pipewire lib32-pipewire-jack lib32-pipewire-v4l2 gst-plugin-pipewire sway ibus-hangul noto-fonts noto-fonts-cjk noto-fonts-emoji xdg-desktop-portal-wlr smplayer smplayer-skins smplayer-themes ffmpegthumbnailer gst-libav gst-plugins-good gst-plugin-va rhythmbox xfce4-terminal libreoffice-fresh-ko gimp pavucontrol firefox-i18n-ko greetd grim slurp swaybg dmenu wofi nautilus xorg-xwayland
 
-systemctl enable greetd
+echo "GTK_IM_MODULE=ibus" >> /etc/environment
+echo "QT_IM_MODULE=ibus" >> /etc/environment
+echo "XMODIFIERS=@im=ibus" >> /etc/environment
+
+#systemctl enable greetd
 
 #사용자 계정 sudo 명령어 설정.
 #pacman -Sy sudo
